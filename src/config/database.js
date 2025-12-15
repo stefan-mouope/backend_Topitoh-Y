ab9228c93daa7718c9a59503fc06dc8372da74be
stefan-mouope <stefan.bouognong@facsciences-uy1.cm>
2025-12-15 13:02:30 +0100

    changement de la congig de database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,23 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-  
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    define: {
-      freezeTableName: true,
-      underscored: false,
-      timestamps: true,
+// Utiliser l'URL complète de Neon
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,          // obligatoire pour Neon
+      rejectUnauthorized: false, // permet de bypass les certificats non vérifiés
     },
-  }
-);
+  },
+  logging: false,
+  define: {
+    freezeTableName: true,
+    underscored: false,
+    timestamps: true,
+  },
+});
 
 // Test de connexion
 sequelize.authenticate()
